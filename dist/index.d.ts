8fc10b31848bb5666a01d5236737bc0569b33217
Nico <nico.kruger@gmail.com>
2023-01-18 06:41:59 +0200

    v0.5.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,36 +1,73 @@
+export interface PromoProduct {
+    productId: string;
+    name?: string;
+    description?: string;
+    image?: string;
+}
+export interface RedeemedProduct {
+    productId: string;
+    status: string;
+    provisionId: string;
+}
+export interface PromoProductProvisioned {
+    productId: string;
+    provisionId: string;
+    status: string;
+    errorMessage?: string;
+}
+export interface InputPromoEligible {
+    code: string;
+}
+export interface ResultPromoEligible {
+    eligibleId?: string;
+    eligible?: boolean;
+    promos?: PromoProduct[];
+    errorMessage?: string;
+}
+export interface InputPromoRedeem {
+    eligibleId: string;
+    msisdn: string;
+}
+export interface ResultPromoRedeem {
+    redeemedId?: string;
+    status: string;
+    promos?: RedeemedProduct[];
+    errorMessage?: string;
+}
 export interface Pet {
     id: number;
     type: string;
     name: string;
     nickname?: string;
+    age?: number;
 }
-export declare namespace Pets {
+export declare namespace Eligible {
     /**
-     * @description Get all pets
-     * @name GetPets
-     * @request GET:/pets
-     * @response `200` `(Pet)[]` Success
+     * @description Checks eligibility of a coupon
+     * @name CouponEligible
+     * @request POST:/eligible
+     * @response `200` `ResultPromoEligible` Success
      */
-    namespace GetPets {
+    namespace CouponEligible {
         type RequestParams = {};
         type RequestQuery = {};
-        type RequestBody = never;
+        type RequestBody = InputPromoEligible;
         type RequestHeaders = {};
-        type ResponseBody = Pet[];
+        type ResponseBody = ResultPromoEligible;
     }
+}
+export declare namespace Redeem {
     /**
-     * @description Get a pet by ID
-     * @name GetPetById
-     * @request GET:/pets/{id}
-     * @response `200` `Pet` Success
+     * @description Redeems a coupon id received from /eligible
+     * @name CouponRedeem
+     * @request POST:/redeem
+     * @response `200` `ResultPromoRedeem` Success
      */
-    namespace GetPetById {
-        type RequestParams = {
-            id: number;
-        };
+    namespace CouponRedeem {
+        type RequestParams = {};
         type RequestQuery = {};
-        type RequestBody = never;
+        type RequestBody = InputPromoRedeem;
         type RequestHeaders = {};
-        type ResponseBody = Pet;
+        type ResponseBody = ResultPromoRedeem;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,38 +9,85 @@
  * ---------------------------------------------------------------
  */
 
+export interface PromoProduct {
+  productId: string;
+  name?: string;
+  description?: string;
+  image?: string;
+}
+
+export interface RedeemedProduct {
+  productId: string;
+  status: string;
+  provisionId: string;
+}
+
+export interface PromoProductProvisioned {
+  productId: string;
+  provisionId: string;
+  status: string;
+  errorMessage?: string;
+}
+
+export interface InputPromoEligible {
+  code: string;
+}
+
+export interface ResultPromoEligible {
+  eligibleId?: string;
+  eligible?: boolean;
+  promos?: PromoProduct[];
+  errorMessage?: string;
+}
+
+export interface InputPromoRedeem {
+  eligibleId: string;
+  msisdn: string;
+}
+
+export interface ResultPromoRedeem {
+  redeemedId?: string;
+  status: string;
+  promos?: RedeemedProduct[];
+  errorMessage?: string;
+}
+
 export interface Pet {
   id: number;
   type: string;
   name: string;
   nickname?: string;
+  age?: number;
 }
 
-export namespace Pets {
+export namespace Eligible {
   /**
-   * @description Get all pets
-   * @name GetPets
-   * @request GET:/pets
-   * @response `200` `(Pet)[]` Success
+   * @description Checks eligibility of a coupon
+   * @name CouponEligible
+   * @request POST:/eligible
+   * @response `200` `ResultPromoEligible` Success
    */
-  export namespace GetPets {
+  export namespace CouponEligible {
     export type RequestParams = {};
     export type RequestQuery = {};
-    export type RequestBody = never;
+    export type RequestBody = InputPromoEligible;
     export type RequestHeaders = {};
-    export type ResponseBody = Pet[];
+    export type ResponseBody = ResultPromoEligible;
   }
+}
+
+export namespace Redeem {
   /**
-   * @description Get a pet by ID
-   * @name GetPetById
-   * @request GET:/pets/{id}
-   * @response `200` `Pet` Success
+   * @description Redeems a coupon id received from /eligible
+   * @name CouponRedeem
+   * @request POST:/redeem
+   * @response `200` `ResultPromoRedeem` Success
    */
-  export namespace GetPetById {
-    export type RequestParams = { id: number };
+  export namespace CouponRedeem {
+    export type RequestParams = {};
     export type RequestQuery = {};
-    export type RequestBody = never;
+    export type RequestBody = InputPromoRedeem;
     export type RequestHeaders = {};
-    export type ResponseBody = Pet;
+    export type ResponseBody = ResultPromoRedeem;
   }
 }
